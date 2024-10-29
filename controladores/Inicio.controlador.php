f280b38d36e5ecaae2d70b8b61ef3ff760cfc523
Jona2005-ac <alvaradomiguel884@gmail.com>
2024-10-29 16:26:39 -0500

    Se agrego plantilla visual para proyecto mvc

--- controladores/Inicio.controlador.php
+++ controladores/Inicio.controlador.php
@@ -1,16 +1,19 @@
 <?php
 
-class InicioControlador{
+
+class InicioControlador {
     private $modelo;
 
-    public function _CONSTRUCT(){
-        $this->modelo=new Producto();
+    public function __construct() {
+        //$this->modelo = new Producto();
     }
 
-    public function Inicio(){
+    public function Inicio() {
         $bd = base_dato::Conectar();
-        require_once "vistas/inicio/principal.php";
-
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/proyecto_prueba/Proyecto_php-main/vistas/encabezado.php'; 
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/proyecto_prueba/Proyecto_php-main/vistas/inicio/principal.php';
+        require_once "../vistas/pie.php";
     }
-} 
+}
+
 
---
+++ vistas/pie.php
@@ -0,0 +1,8 @@
+    <!-- Javascripts-->
+    <script src="assetts/js/jquery-2.1.4.min.js"></script>
+    <script src="assetts/js/bootstrap.min.js"></script>
+    <script src="assetts/js/plugins/pace.min.js"></script>
+    <script src="assetts/js/main.js"></script>
+  </body>
+
+  
